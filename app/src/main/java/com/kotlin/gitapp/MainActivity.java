474104993f55d8e6d2e9a30d518b4269f062a0c2
GurvinderParas <gurvinder.parastechnologies@gmail.com>
2017-11-24 15:44:53 +0530

    changes on MainActivity

--- app/src/main/java/com/kotlin/gitapp/MainActivity.java
+++ app/src/main/java/com/kotlin/gitapp/MainActivity.java
@@ -2,12 +2,18 @@ package com.kotlin.gitapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView abc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        abc = (TextView)findViewById(R.id.abc);
+
     }
 }
